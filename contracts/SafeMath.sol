3704011a54a77fea990e9a0d684315ced7356b19
Desmond Ding <info@desmonding.me>
2019-02-25 16:19:33 +0800

    Enable all core rules for solidity linter and fix style issues
    Test Plan: N/A
    
    Reviewers: tyr034, summerhack
    
    Reviewed By: tyr034
    
    Differential Revision: http://47.244.8.26/D13

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,20 +1,21 @@
 pragma solidity 0.5.0;
 
+
 contract SafeMath {
-    function add(uint256 a,uint256 b)public pure returns(uint256){
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
         uint256 c = a + b;
         require(c > 0);
         return c;
     }
 
-    function sub(uint256 a,uint256 b)public pure returns(uint256){
+    function sub(uint256 a, uint256 b) public pure returns (uint256) {
         require(a > b);
         uint256 c = a - b;
         return c;
     }
 
-    function mul(uint256 a,uint256 b)public pure returns(uint256){
-        if(a == 0){
+    function mul(uint256 a, uint256 b) public pure returns (uint256) {
+        if (a == 0) {
             return 0;
         }
         uint c = a * b;
@@ -22,13 +23,13 @@ contract SafeMath {
         return c;
     }
 
-    function div(uint256 a,uint256 b)public pure returns(uint256){
+    function div(uint256 a, uint256 b) public pure returns (uint256) {
         require(b > 0);
         uint256 c = a / b;
         return c;
     }
     
-    function mod(uint256 a,uint256 b)public pure returns(uint256){
+    function mod(uint256 a, uint256 b) public pure returns (uint256) {
         require(b > 0);
         uint256 c = a % b;
         return c;
