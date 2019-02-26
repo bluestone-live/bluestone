28192f9de34e86f8ba7e814bb643408e1ac95baa
Desmond Ding <info@desmonding.me>
2019-02-26 15:19:15 +0800

    Replace SafeMath with OpenZeppelin's and update FixedMath

--- contracts/FixedMath.sol
+++ contracts/FixedMath.sol
@@ -1,61 +1,64 @@
 pragma solidity 0.5.0;
 pragma experimental ABIEncoderV2;
-import "../contracts/SafeMath.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 
 contract FixedMath {
+    using SafeMath for uint256;
     uint256 precision = 10**18;
+
     struct Fixed {
         uint256 value;
     }
 
     function toFixed(uint a, uint b) public view returns (Fixed memory) {
-        uint c = mul(a, precision);
-        uint d = div(c, b);
+        uint c = a.mul(precision);
+        uint d = c.div(b);
         return Fixed({value: d});
     }
 
     function addFixed(Fixed memory a, Fixed memory b) public pure returns (Fixed memory) {
-        uint256 c = add(a.value, b.value);
+        uint256 c = a.value.add(b.value);
         return Fixed({value:c});
     }
 
     function subFixed(Fixed memory a, Fixed memory b) public pure returns (Fixed memory) {
-        uint256 c = sub(a.value, b.value);
+        uint256 c = a.value.sub(b.value);
         return Fixed({value:c});
     }
 
     function uintMulFixed(uint256 a, Fixed memory b) public pure returns (Fixed memory) {
-        uint256 c = mul(a, b.value);
+        uint256 c = a.mul(b.value);
         return Fixed({value:c});
     }
 
     function fixedMulFixed(Fixed memory a, Fixed memory b) public view returns (Fixed memory) {
-        uint256 c = mul(a.value, b.value);
-        uint256 d = div(c, precision);
+        uint256 c = a.value.mul(b.value);
+        uint256 d = c.div(precision);
         return Fixed({value:d});
     }
 
     function fixedDivFixed(Fixed memory a, Fixed memory b) public view returns (Fixed memory) {
-        uint256 c = mul(a.value, precision);
-        uint256 d = div(c, b.value);
+        uint256 c = a.value.mul(precision);
+        uint256 d = c.div(b.value);
         return Fixed({value:d});
     }
 
     function fixedDivUint(Fixed memory a, uint256 b) public pure returns (Fixed memory) {
-        uint256 c = div(a.value, b);
+        uint256 c = a.value.div(b);
         return Fixed({value:c});
     }
 
     function uintDivFixed(uint256 a, Fixed memory b) public view returns (Fixed memory) {
-        uint256 c = mul(a, precision);
-        uint256 d = mul(c, precision);
-        uint256 e = div(d, b.value);
+        uint256 c = a.mul(precision);
+        uint256 d = c.mul(precision);
+        uint256 e = d.div(b.value);
         return Fixed({value:e});
     }
 
     function uintDivUint(uint256 a, uint256 b) public view returns (Fixed memory) {
-        uint256 c = mul(a, precision);
-        uint256 d = div(c, b);
+        uint256 c = a.mul(precision);
+        uint256 d = c.div(b);
         return Fixed({value:d});
     }
 }
