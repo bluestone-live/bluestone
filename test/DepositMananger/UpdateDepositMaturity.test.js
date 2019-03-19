9c42982f6bcb084e33cc1cf5ebd5d2077c2abc20
Desmond Ding <info@desmonding.me>
2019-03-19 14:49:11 +0800

    [Refactor] Remove redundant tests
    Summary: [Refactor] Remove redundant tests
    
    Test Plan: N/A
    
    Reviewers: tyr034
    
    Reviewed By: tyr034
    
    Differential Revision: http://47.244.8.26/D46

--- test/DepositMananger/UpdateDepositMaturity.test.js
+++ test/DepositMananger/UpdateDepositMaturity.test.js
@@ -8,26 +8,6 @@ contract('DepositManager', ([owner]) => {
       manager = await DepositManager.new()
     })
 
-    it('updates deposit maturity for 1-day-term pool groups', async () => {
-      const term = 1
-      const oneTimeDeposit = 100e18
-      const recurringDeposit = 50e18
-      await manager.addToOneTimeDeposit(owner, term, oneTimeDeposit.toString())
-      await manager.addToRecurringDeposit(owner, term, recurringDeposit.toString())
-
-      const prevOneTimeDeposit = await manager.getOneTimeDepositFromPool(term, 1)
-      const prevRecurringDeposit = await manager.getRecurringDepositFromPool(term, 1)
-      assert.equal(prevOneTimeDeposit, oneTimeDeposit)
-      assert.equal(prevRecurringDeposit, recurringDeposit)
-
-      await manager.updateDepositMaturity()
-
-      const currOneTimeDeposit = await manager.getOneTimeDepositFromPool(term, 1)
-      const currRecurringDeposit = await manager.getRecurringDepositFromPool(term, 1)
-      assert.equal(currOneTimeDeposit, 0)
-      assert.equal(currRecurringDeposit, recurringDeposit)
-    })
-
     it('updates deposit maturity for 7-day-term pool groups', async () => {
       const term = 7
       const oneTimeDeposit = 100e18
--- test/PoolGroup/IncrementPoolIndexes.test.js
+++ test/PoolGroup/IncrementPoolIndexes.test.js
@@ -5,13 +5,6 @@ contract('PoolGroup', () => {
     it('increments pool indexes correctly after one day', async () => {
       const term = 7
       const poolGroup = await PoolGroup.new(term)
-      const initialPoolIndexes = [0, 1, 2, 3, 4, 5, 6]
-
-      for (let i = 0; i < term; i++) {
-        const poolIndex = await poolGroup.poolIndexes(i)
-        assert.equal(poolIndex, initialPoolIndexes[i])
-      }
-
       await poolGroup.incrementPoolIndexes()
       const updatedPoolIndexes = [1, 2, 3, 4, 5, 6, 0]
             
@@ -24,12 +17,7 @@ contract('PoolGroup', () => {
     it('increments pool indexes correctly after seven days', async () => {
       const term = 7
       const poolGroup = await PoolGroup.new(term)
-      const initialPoolIndexes = [0, 1, 2, 3, 4, 5, 6]
-
-      for (let i = 0; i < term; i++) {
-        const poolIndex = await poolGroup.poolIndexes(i)
-        assert.equal(poolIndex, initialPoolIndexes[i])
-      }
+      const updatedPoolIndexes = [0, 1, 2, 3, 4, 5, 6]
 
       for (let i = 0; i < term; i++) {
         await poolGroup.incrementPoolIndexes()
@@ -37,7 +25,7 @@ contract('PoolGroup', () => {
 
       for (let i = 0; i < term; i++) {
         const poolIndex = await poolGroup.poolIndexes(i)
-        assert.equal(poolIndex, initialPoolIndexes[i])
+        assert.equal(poolIndex, updatedPoolIndexes[i])
       }
     })
   })
