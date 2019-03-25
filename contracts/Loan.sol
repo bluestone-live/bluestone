44777507e357c583b815e7bce61d550dbb44cab4
Desmond Ding <info@desmonding.me>
2019-03-25 16:03:52 +0800

    [Loan] Add simple test for setRecord

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -8,6 +8,8 @@ contract Loan {
     uint private _collateralAmount;
 
     /// Deposit Term -> Pool Term -> Amount
+    /// How much have one loaned from a specific pool (identified by Pool Term) 
+    /// in a specific pool group (identified by Deposit Term).
     mapping(uint8 => mapping(uint8 => uint)) private records;
 
     constructor(address owner, uint8 term, uint loanAmount, uint collateralAmount) public {
@@ -15,6 +17,10 @@ contract Loan {
         _term = term;
         _loanAmount = loanAmount;
         _collateralAmount = collateralAmount;
+    }
+
+    function getRecord(uint8 depositTerm, uint8 poolTerm) external view returns (uint) {
+        return records[depositTerm][poolTerm];
     }
 
     function setRecord(uint8 depositTerm, uint8 poolTerm, uint amount) external {
---
+++ test/Loan/SetRecord.test.js
@@ -0,0 +1,19 @@
+const Loan = artifacts.require('Loan')
+const { shouldFail } = require('openzeppelin-test-helpers')
+
+contract('Loan', ([owner, anotherAccount]) => {
+  const term = 7
+  const loanAmount = 100e18.toString()
+  const collateralAmount = 300e18.toString()
+
+  describe('setRecord', () => {
+    it('succeeds', async () => {
+      const loan = await Loan.new(owner, term, loanAmount, collateralAmount)
+      await loan.setRecord(7, 1, 70e18.toString()) 
+      await loan.setRecord(7, 2, 30e18.toString())
+      assert.equal((await loan.getRecord(7, 1)), 70e18.toString())
+      assert.equal((await loan.getRecord(7, 2)), 30e18.toString())
+    })
+  })
+})
+
