af378981fcd9940ece2351640addb0c1600ad73a
Desmond Ding <info@desmonding.me>
2019-04-25 16:31:30 +0800

    [DateTime] Implement utility methods for Unix timestamp conversion
    Summary: Implement utility methods for Unix timestamp conversion, which will be later used to set exact maturity date of a deposit.
    
    Test Plan: Tested.
    
    Reviewers: tyr034
    
    Reviewed By: tyr034
    
    Tags: #smart_contract_development
    
    Differential Revision: http://47.244.8.26/D73

---
+++ contracts/DateTime.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.0;
+
+
+/// Unix timestamp conversion utilities
+/// To learn more, try this online converter: https://www.epochconverter.com/
+library DateTime {
+    uint constant DAY_IN_SECONDS = 86400;
+    uint constant HOUR_IN_SECONDS = 3600;
+    uint constant MINUTE_IN_SECONDS = 60;
+
+    function getHour(uint timestamp) public pure returns (uint8) {
+        return uint8((timestamp / 60 / 60) % 24);
+    }
+
+    function getMinute(uint timestamp) public pure returns (uint8) {
+        return uint8((timestamp / 60) % 60);
+    }
+
+    function getSecond(uint timestamp) public pure returns (uint8) {
+        return uint8(timestamp % 60);
+    }
+
+    function secondsUntilMidnight(uint timestamp) public pure returns (uint) {
+        uint8 hour = getHour(timestamp);
+        uint8 minute = getMinute(timestamp);
+        uint8 second = getSecond(timestamp);
+        
+        return DAY_IN_SECONDS - hour * HOUR_IN_SECONDS - minute * MINUTE_IN_SECONDS - second;
+    }
+}
---
+++ test/unit/DateTime.test.js
@@ -0,0 +1,55 @@
+const DateTime = artifacts.require('DateTime')
+const { BN } = require('openzeppelin-test-helpers')
+const { expect } = require('chai')
+
+const DAY_IN_SECONDS = 86400
+
+contract('DateTime', () => {
+  let datetime
+
+  before(async () => {
+    datetime = await DateTime.new()
+  })
+
+  describe('#getHour', () => {
+    require('./testcases/DateTime/getHour.js').forEach(({args, expected}) => {
+      it(`returns ${expected}`, async () => {
+        expect(await datetime.getHour.apply(null, args)).to.be.bignumber.equal(new BN(expected))
+      })
+    })
+  })
+
+  describe('#getMinute', () => {
+    require('./testcases/DateTime/getMinute.js').forEach(({args, expected}) => {
+      it(`returns ${expected}`, async () => {
+        expect(await datetime.getMinute.apply(null, args)).to.be.bignumber.equal(new BN(expected))
+      })
+    })
+  })
+
+  describe('#getSecond', () => {
+    require('./testcases/DateTime/getSecond.js').forEach(({args, expected}) => {
+      it(`returns ${expected}`, async () => {
+        expect(await datetime.getSecond.apply(null, args)).to.be.bignumber.equal(new BN(expected))
+      })
+    })
+  })
+
+  describe('#secondsUntilMidnight', () => {
+    it('returns 1', async () => {
+      // Friday, December 31, 1971 11:59:59 PM
+      expect(await datetime.secondsUntilMidnight(63071999)).to.be.bignumber.equal(new BN(1))
+    })
+
+    it('returns 36001', async () => {
+      // Saturday, January 1, 1972 1:59:59 PM
+      const expected = DAY_IN_SECONDS - 13 * 3600 - 59 * 60 - 59
+      expect(await datetime.secondsUntilMidnight(63122399)).to.be.bignumber.equal(new BN(expected))
+    })
+
+    it('returns 86400', async () => {
+      // Saturday, January 1, 1972 12:00:00 AM
+      expect(await datetime.secondsUntilMidnight(63072000)).to.be.bignumber.equal(new BN(DAY_IN_SECONDS))
+    })
+  })
+})
