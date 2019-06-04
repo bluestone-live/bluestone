fa6fd52f927fe9dbca82e1f62bfeeed68d20362f
Desmond Ding <info@desmonding.me>
2019-06-04 09:06:29 +0800

    [Client] Add Anchor component
    Test Plan: N/A
    
    Reviewers: ZhangRGK
    
    Reviewed By: ZhangRGK
    
    Differential Revision: http://47.244.8.26/D126

---
+++ client/src/components/html/Anchor.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import styled, { css } from 'styled-components';
+
+interface IProps {
+  children: React.ReactChildren;
+  href: string;
+}
+
+const StyledAnchor = styled.a`
+  color: ${props => props.theme.fontColor};
+  text-decoration: none;
+  cursor: pointer;
+
+  :hover {
+    color: ${props => props.theme.primaryColor};
+  }
+`;
+
+const Anchor = (props: IProps) => {
+  const { children } = props;
+  return <StyledAnchor {...props}>{children}</StyledAnchor>;
+};
+
+export default Anchor;
