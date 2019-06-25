febf3c459df2d34e0208873f51de2ae4e96fb471
Desmond Ding <info@desmonding.me>
2019-06-25 16:31:57 -0700

    [Script] Update initial supply and allowance so the account has zero allowance at first
    Summary: Note: prettier formatted.
    
    Reviewers: ZhangRGK
    
    Reviewed By: ZhangRGK
    
    Differential Revision: http://47.244.8.26/D170

--- scripts/javascript/setupEnvironment.js
+++ scripts/javascript/setupEnvironment.js
@@ -1,126 +1,172 @@
-const debug = require('debug')('script:setupEnvironment')
-const TokenFactory = artifacts.require('./TokenFactory.sol') 
-const DepositManager = artifacts.require("./DepositManager.sol")
-const LoanManager = artifacts.require("./LoanManager.sol")
-const Configuration = artifacts.require('./Configuration.sol')
-const PriceOracle = artifacts.require('./PriceOracle.sol')
-const ERC20Mock = artifacts.require('./ERC20Mock.sol') 
-const TokenManager = artifacts.require('./TokenManager.sol')
-const { makeTruffleScript } = require('./utils.js')
-const { configuration } = require('../../config.js')
-const { BN } = web3.utils
+const debug = require("debug")("script:setupEnvironment");
+const TokenFactory = artifacts.require("./TokenFactory.sol");
+const DepositManager = artifacts.require("./DepositManager.sol");
+const LoanManager = artifacts.require("./LoanManager.sol");
+const Configuration = artifacts.require("./Configuration.sol");
+const PriceOracle = artifacts.require("./PriceOracle.sol");
+const ERC20Mock = artifacts.require("./ERC20Mock.sol");
+const TokenManager = artifacts.require("./TokenManager.sol");
+const { makeTruffleScript } = require("./utils.js");
+const { configuration } = require("../../config.js");
+const { BN } = web3.utils;
 
 module.exports = makeTruffleScript(async () => {
   if (!isValidConfiguartion(configuration)) {
-    throw 'Invalid configuration. Check your ./config.js file.'
+    throw "Invalid configuration. Check your ./config.js file.";
   }
 
-  const tokenFactory = await TokenFactory.deployed()
-  const depositManager = await DepositManager.deployed()
-  const loanManager = await LoanManager.deployed()
-  const config = await Configuration.deployed()
-  const { tokenList, collateralRatio, liquidationDiscount, loanInterestRate } = configuration
+  const tokenFactory = await TokenFactory.deployed();
+  const depositManager = await DepositManager.deployed();
+  const loanManager = await LoanManager.deployed();
+  const config = await Configuration.deployed();
+  const {
+    tokenList,
+    collateralRatio,
+    liquidationDiscount,
+    loanInterestRate
+  } = configuration;
 
-  let tokenAddressMap = {}
+  let tokenAddressMap = {};
 
   for (let token of tokenList) {
-    const { name, symbol } = token 
-    const { logs } = await tokenFactory.createToken(name, symbol)
-    const address = logs
-      .filter(({ event }) => event === 'TokenCreated')[0]
-      .args['token']
+    const { name, symbol } = token;
+    const { logs } = await tokenFactory.createToken(name, symbol);
+    const address = logs.filter(({ event }) => event === "TokenCreated")[0]
+      .args["token"];
 
-    debug(`Deployed ${symbol} at ${address}`)
-    tokenAddressMap[symbol] = address
+    debug(`Deployed ${symbol} at ${address}`);
+    tokenAddressMap[symbol] = address;
   }
 
-  const tokenSymbolList = Object.keys(tokenAddressMap)
+  const tokenSymbolList = Object.keys(tokenAddressMap);
 
   for (let loanTokenSymbol of tokenSymbolList) {
-    divider()
+    divider();
 
-    const loanAsset = tokenAddressMap[loanTokenSymbol]
-    const loanTerms = [1, 7, 30]
+    const loanAsset = tokenAddressMap[loanTokenSymbol];
+    const loanTerms = [1, 7, 30];
 
-    await depositManager.enableDepositAsset(loanAsset)
-    debug(`enableDepositAsset: ${loanTokenSymbol}`)
+    await depositManager.enableDepositAsset(loanAsset);
+    debug(`enableDepositAsset: ${loanTokenSymbol}`);
 
     for (let loanTerm of loanTerms) {
-      const decimalLoanInterestRate = loanInterestRate[loanTokenSymbol][loanTerm]
-      await config.setLoanInterestRate(loanAsset, loanTerm, toFixedBN(decimalLoanInterestRate)) 
-      debug(`setLoanInterestRate: ${loanTokenSymbol} ${loanTerm} ${decimalLoanInterestRate}`)
+      const decimalLoanInterestRate =
+        loanInterestRate[loanTokenSymbol][loanTerm];
+      await config.setLoanInterestRate(
+        loanAsset,
+        loanTerm,
+        toFixedBN(decimalLoanInterestRate)
+      );
+      debug(
+        `setLoanInterestRate: ${loanTokenSymbol} ${loanTerm} ${decimalLoanInterestRate}`
+      );
     }
 
     for (let collateralTokenSymbol of tokenSymbolList) {
-      const collateralAsset = tokenAddressMap[collateralTokenSymbol]
+      const collateralAsset = tokenAddressMap[collateralTokenSymbol];
 
       if (loanAsset !== collateralAsset) {
-        await loanManager.enableLoanAssetPair(loanAsset, collateralAsset)
-        debug(`enableLoanAssetPair: ${loanTokenSymbol} ${collateralTokenSymbol}`)
-        
-        const decimalCollateralRatio = collateralRatio[loanTokenSymbol][collateralTokenSymbol]
-        await config.setCollateralRatio(loanAsset, collateralAsset, toFixedBN(decimalCollateralRatio))
-        debug(`setCollateralRatio: ${loanTokenSymbol} ${collateralTokenSymbol} ${decimalCollateralRatio}`)
+        await loanManager.enableLoanAssetPair(loanAsset, collateralAsset);
+        debug(
+          `enableLoanAssetPair: ${loanTokenSymbol} ${collateralTokenSymbol}`
+        );
 
-        const decimalLiquidationDiscount = liquidationDiscount[loanTokenSymbol][collateralTokenSymbol]
-        await config.setLiquidationDiscount(loanAsset, collateralAsset, toFixedBN(decimalLiquidationDiscount))
-        debug(`setLiquidationDiscount: ${loanTokenSymbol} ${collateralTokenSymbol} ${decimalLiquidationDiscount}`)
+        const decimalCollateralRatio =
+          collateralRatio[loanTokenSymbol][collateralTokenSymbol];
+        await config.setCollateralRatio(
+          loanAsset,
+          collateralAsset,
+          toFixedBN(decimalCollateralRatio)
+        );
+        debug(
+          `setCollateralRatio: ${loanTokenSymbol} ${collateralTokenSymbol} ${decimalCollateralRatio}`
+        );
+
+        const decimalLiquidationDiscount =
+          liquidationDiscount[loanTokenSymbol][collateralTokenSymbol];
+        await config.setLiquidationDiscount(
+          loanAsset,
+          collateralAsset,
+          toFixedBN(decimalLiquidationDiscount)
+        );
+        debug(
+          `setLiquidationDiscount: ${loanTokenSymbol} ${collateralTokenSymbol} ${decimalLiquidationDiscount}`
+        );
       }
     }
   }
 
-  divider()
+  divider();
 
-  const account = web3.eth.accounts.create()
-  const { address } = account
+  const account = web3.eth.accounts.create();
+  const { address } = account;
 
-  debug(`setShareholderAddress: ${address}`)
-  await config.setShareholderAddress(address)
+  debug(`setShareholderAddress: ${address}`);
+  await config.setShareholderAddress(address);
 
-  divider()
+  divider();
 
   // Use fake price here so we know the exact price for later calculation
-  const priceList = [ 10, 10, 10 ]
-  const scaledPriceList = priceList.map(price => toFixedBN(price))
-  const priceOracle = await PriceOracle.deployed()
-  const tokenAddressList = tokenSymbolList.map(tokenSymbol => tokenAddressMap[tokenSymbol])
+  const priceList = [10, 10, 10];
+  const scaledPriceList = priceList.map(price => toFixedBN(price));
+  const priceOracle = await PriceOracle.deployed();
+  const tokenAddressList = tokenSymbolList.map(
+    tokenSymbol => tokenAddressMap[tokenSymbol]
+  );
 
-  debug(`setPrices: ${tokenSymbolList} ${priceList}`)
-  await priceOracle.setPrices(tokenAddressList, scaledPriceList)
+  debug(`setPrices: ${tokenSymbolList} ${priceList}`);
+  await priceOracle.setPrices(tokenAddressList, scaledPriceList);
 
-  divider()
+  divider();
 
-  debug(`Initialize deposits for each asset and loans for each asset pair`)
-  const tokenManager = await TokenManager.deployed()
-  const [ owner, depositor, loaner ] = await web3.eth.getAccounts()
-  const initialSupply = toFixedBN(3000)
-  const depositAmount = 1000
-  const loanAmount = 100
-  const collateralAmount = 200
-  const freedCollateralAmount = 0
-  const terms = [1, 7, 30]
+  debug(`Initialize deposits for each asset and loans for each asset pair`);
+  const tokenManager = await TokenManager.deployed();
+  const [owner, depositor, loaner] = await web3.eth.getAccounts();
+  const initialSupply = toFixedBN(6000);
+  const initialAllowance = toFixedBN(3000);
+  const depositAmount = 1000;
+  const loanAmount = 100;
+  const collateralAmount = 200;
+  const freedCollateralAmount = 0;
+  const terms = [1, 7, 30];
 
   for (let loanTokenSymbol of tokenSymbolList) {
-    divider()
+    divider();
 
-    const loanAsset = await ERC20Mock.at(tokenAddressMap[loanTokenSymbol])
-    await loanAsset.mint(depositor, initialSupply)
+    const loanAsset = await ERC20Mock.at(tokenAddressMap[loanTokenSymbol]);
+    await loanAsset.mint(depositor, initialSupply);
 
-    await loanAsset.approve(tokenManager.address, initialSupply, { from: depositor })
-    debug(`Depositor approves sending ${loanTokenSymbol} to protocol`)
+    await loanAsset.approve(tokenManager.address, initialAllowance, {
+      from: depositor
+    });
+    debug(`Depositor approves sending ${loanTokenSymbol} to protocol`);
 
     for (let term of terms) {
-      await depositManager.deposit(loanAsset.address, term, toFixedBN(depositAmount), false, { from: depositor })
-      debug(`Depositor deposits ${depositAmount} ${loanTokenSymbol} in ${term}-day term`)
+      await depositManager.deposit(
+        loanAsset.address,
+        term,
+        toFixedBN(depositAmount),
+        false,
+        { from: depositor }
+      );
+      debug(
+        `Depositor deposits ${depositAmount} ${loanTokenSymbol} in ${term}-day term`
+      );
     }
 
     for (let collateralTokenSymbol of tokenSymbolList) {
       if (collateralTokenSymbol !== loanTokenSymbol) {
-        const collateralAsset = await ERC20Mock.at(tokenAddressMap[collateralTokenSymbol])
-        await collateralAsset.mint(loaner, initialSupply)
+        const collateralAsset = await ERC20Mock.at(
+          tokenAddressMap[collateralTokenSymbol]
+        );
+        await collateralAsset.mint(loaner, initialSupply);
 
-        await collateralAsset.approve(tokenManager.address, initialSupply, { from: loaner })
-        debug(`Loaner approves sending collateral ${collateralTokenSymbol} to protocol`)
+        await collateralAsset.approve(tokenManager.address, initialAllowance, {
+          from: loaner
+        });
+        debug(
+          `Loaner approves sending collateral ${collateralTokenSymbol} to protocol`
+        );
 
         for (let term of terms) {
           const { logs } = await loanManager.loan(
@@ -131,51 +177,75 @@ module.exports = makeTruffleScript(async () => {
             toFixedBN(collateralAmount),
             freedCollateralAmount,
             { from: loaner }
-          )
-          debug(`Loaner loans ${loanAmount} ${loanTokenSymbol} using ${collateralAmount} ${collateralTokenSymbol} collateral in ${term}-day term`)
+          );
+          debug(
+            `Loaner loans ${loanAmount} ${loanTokenSymbol} using ${collateralAmount} ${collateralTokenSymbol} collateral in ${term}-day term`
+          );
         }
       }
     }
 
     // Now we have initial deposits and loans distributed evenly, we shall set initial coefficients
-    setCoefficient(loanTokenSymbol, loanAsset.address, 1, 1, 0.33)
-    setCoefficient(loanTokenSymbol, loanAsset.address, 7, 1, 0.33)
-    setCoefficient(loanTokenSymbol, loanAsset.address, 30, 1, 0.34)
-    setCoefficient(loanTokenSymbol, loanAsset.address, 7, 7, 0.5)
-    setCoefficient(loanTokenSymbol, loanAsset.address, 30, 7, 0.5)
-    setCoefficient(loanTokenSymbol, loanAsset.address, 30, 30, 1)
+    setCoefficient(loanTokenSymbol, loanAsset.address, 1, 1, 0.33);
+    setCoefficient(loanTokenSymbol, loanAsset.address, 7, 1, 0.33);
+    setCoefficient(loanTokenSymbol, loanAsset.address, 30, 1, 0.34);
+    setCoefficient(loanTokenSymbol, loanAsset.address, 7, 7, 0.5);
+    setCoefficient(loanTokenSymbol, loanAsset.address, 30, 7, 0.5);
+    setCoefficient(loanTokenSymbol, loanAsset.address, 30, 30, 1);
   }
-})
+});
 
 function divider() {
-  debug(`------------------------------------------------------------------`)
+  debug(`------------------------------------------------------------------`);
 }
 
-// TODO: have to duplicate this function as of now since `truffle exec` environment 
-// seems to have trouble handling require() statement correctly: 
+// TODO: have to duplicate this function as of now since `truffle exec` environment
+// seems to have trouble handling require() statement correctly:
 // https://github.com/trufflesuite/truffle/issues/255
 // const { toFixedBN } = require('../../test/utils/index.js')
 function toFixedBN(num, significant = 18) {
-  let decimalPlaces = (num.toString().split('.')[1] || []).length
+  let decimalPlaces = (num.toString().split(".")[1] || []).length;
 
   if (decimalPlaces === 0) {
-    return new BN(num).mul(new BN(10).pow(new BN(significant)))
+    return new BN(num).mul(new BN(10).pow(new BN(significant)));
   } else {
-    const integer = num * Math.pow(10, decimalPlaces)
-    return new BN(integer).mul(new BN(10).pow(new BN(significant - decimalPlaces)))
+    const integer = num * Math.pow(10, decimalPlaces);
+    return new BN(integer).mul(
+      new BN(10).pow(new BN(significant - decimalPlaces))
+    );
   }
 }
 
 function isValidConfiguartion(configuration) {
   if (configuration) {
-    const { tokenList, collateralRatio, liquidationDiscount, loanInterestRate } = configuration
-    return tokenList && collateralRatio && liquidationDiscount && loanInterestRate
+    const {
+      tokenList,
+      collateralRatio,
+      liquidationDiscount,
+      loanInterestRate
+    } = configuration;
+    return (
+      tokenList && collateralRatio && liquidationDiscount && loanInterestRate
+    );
   } else {
-    return false
+    return false;
   }
 }
 
-async function setCoefficient(tokenSymbol, tokenAddress, depositTerm, loanTerm, decimalValue) {
-  debug(`setCoefficient: ${tokenSymbol} ${depositTerm} ${loanTerm} ${decimalValue}`)
-  await config.setCoefficient(tokenAddress, depositTerm, loanTerm, toFixedBN(decimalValue))
+async function setCoefficient(
+  tokenSymbol,
+  tokenAddress,
+  depositTerm,
+  loanTerm,
+  decimalValue
+) {
+  debug(
+    `setCoefficient: ${tokenSymbol} ${depositTerm} ${loanTerm} ${decimalValue}`
+  );
+  await config.setCoefficient(
+    tokenAddress,
+    depositTerm,
+    loanTerm,
+    toFixedBN(decimalValue)
+  );
 }
