5bd87755ad80d929b7746f1c4f6d43ac86a7753a
ZhangRGK <stamen0630@gmail.com>
2019-11-07 16:25:01 +0800

    [Script]: Set max loan term per token
    Reviewers: desmonding, lambda, tyr034, lu0ty
    
    Reviewed By: desmonding
    
    Differential Revision: https://phabricator.bluestone.live/D463

---
+++ _scripts/javascript/setMaxLoanTerm.js
@@ -0,0 +1,18 @@
+const debug = require('debug')('script:setMaxLoanTerm');
+const ERC20Mock = artifacts.require('./ERC20Mock.sol');
+const Protocol = artifacts.require('./Protocol.sol');
+const { loadConfig, makeTruffleScript } = require('../utils.js');
+
+module.exports = makeTruffleScript(async network => {
+  const { tokens } = loadConfig(network);
+  const tokenSymbolList = Object.keys(tokens);
+  for (symbol of tokenSymbolList) {
+    let token = tokens[symbol];
+    if (!token.tokenAddress) {
+      return debug(`${symbol} is not deployed yet.`);
+    }
+    const protocol = await Protocol.deployed();
+    await protocol.setMaxLoanTerm(token.tokenAddress, token.maxLoanTerm);
+    debug(`Set max loan term: ${token.tokenSymbol} - ${token.maxLoanTerm}`);
+  }
+});
