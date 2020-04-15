9cf30ee6d2038c24824c8db8628aa684dbe8e298
UnsignedInt8 <unsignedint8@gmail.com>
2020-04-15 10:59:03 +0800

    [Client] Replace functions with ethers library
    Summary: Fix T328
    
    Reviewers: desmonding, lu0ty, ZhangRGK, tyr034
    
    Reviewed By: desmonding
    
    Maniphest Tasks: T328
    
    Differential Revision: https://phabricator.bluestone.live/D716

--- client/src/utils/BigNumber.ts
+++ client/src/utils/BigNumber.ts
@@ -1,39 +1,20 @@
 import BigNumber from 'bn.js';
+import { utils } from 'ethers';
 
 export { BigNumber };
-
-const significant = 18;
 
 export const convertWeiToDecimal = (
   bn?: BigNumber | string,
   precision: number = 4,
+  decimals = 18,
 ) => {
   if (!bn) {
     return '0';
   }
-  if (typeof bn === 'string') {
-    bn = new BigNumber(bn);
-  }
-  const numberString = bn.toString();
 
-  const padString = numberString.padStart(
-    numberString.length + significant,
-    '0',
+  return Number.parseFloat(utils.formatUnits(`${bn}`, decimals)).toFixed(
+    precision,
   );
-
-  const integerPlaces = Number.parseInt(
-    padString.substring(0, padString.length - significant),
-    10,
-  ).toString();
-
-  const decimalPlaces = padString.substring(
-    padString.length - significant,
-    padString.length,
-  );
-
-  return `${integerPlaces}.${decimalPlaces
-    .substring(decimalPlaces.length - significant, decimalPlaces.length)
-    .substring(0, precision)}`;
 };
 
 /**
@@ -44,24 +25,7 @@ export const convertWeiToDecimal = (
  * toFixed(5, 16) -> 5e16
  */
 export const convertDecimalToWei = (num: number | string): string => {
-  if (!num) {
-    return '0';
-  }
-
-  const fixedNumber = typeof num === 'number' ? num.toFixed(significant) : num;
-  const decimalPlaces = (fixedNumber.split('.')[1] || '').replace(/0*$/, '')
-    .length;
-
-  if (decimalPlaces === 0) {
-    return new BigNumber(num)
-      .mul(new BigNumber(10).pow(new BigNumber(significant)))
-      .toString();
-  } else {
-    const integer = fixedNumber.replace(/0*$/, '').replace('.', '');
-    return new BigNumber(integer)
-      .mul(new BigNumber(10).pow(new BigNumber(significant - decimalPlaces)))
-      .toString();
-  }
+  return utils.parseEther(`${num || 0}`).toString();
 };
 
 export const ZERO = new BigNumber(0);
