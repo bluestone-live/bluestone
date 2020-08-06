54e4bb98ec1f094bd01bb6fc007e9ed3688e1050
UnsignedInt8 <unsignedint8@gmail.com>
2020-08-06 16:44:01 +0800

    [Script] fix disableTerm
    Summary: Remove if statment
    
    Reviewers: lu0ty, tyr034
    
    Reviewed By: lu0ty
    
    Differential Revision: https://phabricator.bluestone.live/D826

--- scripts/javascript/disableDepositTerm.js
+++ scripts/javascript/disableDepositTerm.js
@@ -4,11 +4,7 @@ const { makeTruffleScript } = require('../utils.js');
 const config = require('config');
 
 module.exports = makeTruffleScript(async (network, term) => {
-  const { depositTerms } = config.get('contract');
-
-  if (depositTerms.indexOf(term) > -1) {
-    const protocol = await Protocol.deployed();
-    debug(`disable deposit term: ${term}`);
-    await protocol.disableDepositTerm(term);
-  }
+  const protocol = await Protocol.deployed();
+  debug(`disable deposit term: ${term}`);
+  await protocol.disableDepositTerm(term);
 });
