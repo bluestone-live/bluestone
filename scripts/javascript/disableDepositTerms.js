7d5373b51306e0c321cd87da523e43a4316886f3
Tianyou Luo <luotywork@gmail.com>
2021-01-16 23:25:36 -0500

    [Script] Remove upgradeable code from disableDepositTerms.js

--- scripts/javascript/disableDepositTerms.js
+++ scripts/javascript/disableDepositTerms.js
@@ -1,16 +1,9 @@
 const debug = require('debug')('script:disableDepositTerms');
 const Protocol = artifacts.require('./Protocol.sol');
-const { makeTruffleScript, loadNetwork } = require('../utils.js');
-const config = require('config');
+const { makeTruffleScript } = require('../utils.js');
 
 module.exports = makeTruffleScript(async (network, ...terms) => {
-  const { depositTerms } = config.get('contract');
-  const {
-    contracts: { OwnedUpgradeabilityProxy: proxyAddress },
-  } = loadNetwork(network);
-
-  const protocol = await Protocol.at(proxyAddress);
-
   debug(`Disable deposit terms: ${terms}`);
+  const protocol = await Protocol.deployed();
   await protocol.disableDepositTerms(terms);
 });
