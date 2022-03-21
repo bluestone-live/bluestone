306bdae355c99db61149ec0ac0523487826cda07
Tianyou Luo <luotywork@gmail.com>
2022-03-20 22:55:37 -0400

    Fix deployment script for loan manager

--- migrations/6_deploy_loan_manager.js
+++ migrations/6_deploy_loan_manager.js
@@ -2,6 +2,6 @@ const Protocol = artifacts.require('Protocol');
 const LoanManager = artifacts.require('LoanManager');
 
 module.exports = async function (deployer) {
-  // await deployer.deploy(LoanManager);
+  await deployer.deploy(LoanManager);
   await deployer.link(LoanManager, [Protocol]);
 };
