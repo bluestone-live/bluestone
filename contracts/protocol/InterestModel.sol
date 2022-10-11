1a089ff99ba97b3633aea13aec9a94249bfe91bc
CanvasL <746591811@qq.com>
2022-10-11 14:54:24 +0800

    feat: change interest rate model

--- contracts/protocol/InterestModel.sol
+++ contracts/protocol/InterestModel.sol
@@ -7,10 +7,9 @@ import './interface/IInterestModel.sol';
 contract InterestModel is IInterestModel, Ownable {
     mapping(address => LoanParameters) _loanParametersByTokenAddress;
 
-    // Parameters to help calculate loan interest rate
     struct LoanParameters {
-        uint256 loanInterestRateLowerBound;
-        uint256 loanInterestRateUpperBound;
+        uint256[] loanTerms;
+        mapping(uint256 => uint256) interestRateByLoanTerm;
     }
 
     function getDepositWeight(uint256 amount, uint256 term)
@@ -27,39 +26,63 @@ contract InterestModel is IInterestModel, Ownable {
         uint256 loanTerm,
         uint256 maxLoanTerm
     ) external view override returns (uint256 loanInterestRate) {
-        LoanParameters memory params = _loanParametersByTokenAddress[
-            tokenAddress
-        ];
-        uint256 H = params.loanInterestRateUpperBound;
-        uint256 L = params.loanInterestRateLowerBound;
-
-        return H - (((H - L) * loanTerm) / maxLoanTerm);
+        require(
+            loanTerm <= maxLoanTerm,
+            'InterestModel: Loan term exceeds max value'
+        );
+        require(
+            !_isLoanTermExist(tokenAddress, loanTerm),
+            "InterestModel: Loan interest rate haven't setted"
+        );
+        return
+            _loanParametersByTokenAddress[tokenAddress].interestRateByLoanTerm[
+                loanTerm
+            ];
     }
 
     function getLoanParameters(address tokenAddress)
         external
         view
-        returns (
-            uint256 loanInterestRateLowerBound,
-            uint256 loanInterestRateUpperBound
-        )
+        returns (uint256[] memory loanTerms, uint256[] memory loanInterestRates)
     {
-        return (
-            _loanParametersByTokenAddress[tokenAddress]
-                .loanInterestRateLowerBound,
-            _loanParametersByTokenAddress[tokenAddress]
-                .loanInterestRateUpperBound
-        );
+        LoanParameters storage loanParameters = _loanParametersByTokenAddress[
+            tokenAddress
+        ];
+        loanTerms = loanParameters.loanTerms;
+        for (uint256 i = 0; i < loanParameters.loanTerms.length; i++) {
+            loanInterestRates[i] = loanParameters.interestRateByLoanTerm[
+                loanParameters.loanTerms[i]
+            ];
+        }
     }
 
     function setLoanParameters(
         address tokenAddress,
-        uint256 loanInterestRateLowerBound,
-        uint256 loanInterestRateUpperBound
+        uint256 loanTerm,
+        uint256 loanInterestRate
     ) external onlyOwner {
-        _loanParametersByTokenAddress[tokenAddress] = LoanParameters({
-            loanInterestRateLowerBound: loanInterestRateLowerBound,
-            loanInterestRateUpperBound: loanInterestRateUpperBound
-        });
+        LoanParameters storage loanParameters = _loanParametersByTokenAddress[
+            tokenAddress
+        ];
+        if (!_isLoanTermExist(tokenAddress, loanTerm)) {
+            loanParameters.loanTerms.push(loanTerm);
+        }
+        loanParameters.interestRateByLoanTerm[loanTerm] = loanInterestRate;
+    }
+
+    function _isLoanTermExist(address tokenAddress, uint256 loanTerm)
+        private
+        view
+        returns (bool)
+    {
+        LoanParameters storage loanParameters = _loanParametersByTokenAddress[
+            tokenAddress
+        ];
+        for (uint256 i = 0; i < loanParameters.loanTerms.length; i++) {
+            if (loanParameters.loanTerms[i] == loanTerm) {
+                return true;
+            }
+        }
+        return false;
     }
 }
